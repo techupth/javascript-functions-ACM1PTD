77bacef7b6d7a88cccaf46cc65f33bea0fb8d31b
Nonthawath Patchakapati <npc2540@hotmail.com>
2024-04-01 23:20:51 +0700

    add answers and debugging

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+
+let add = function(a, b) {
+    let result = a + b
+    return result
+}
+
+let subtract = function(a, b) {
+    let result = a - b
+    return result
+}
+
+let multiply = function(a, b) {
+    let result = a * b
+    return result
+}
+
+let divide = function(a, b) {
+    let result = a / b
+    return result
+}
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
+
+function findMinNumber(a, b) {
+    console.log(Math.min(a, b))
+}
